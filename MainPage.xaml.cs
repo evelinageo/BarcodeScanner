c400b0ec647663241c0324ce4a147d189b8a205d
Evelina Georgiou <107413942+evelinageo@users.noreply.github.com>
2023-03-24 22:47:42 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ZXing.Net.Maui;
+
+namespace BarcodeScanner;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		barcodeRead.Options = new ZXing.Net.Maui.BarcodeReaderOptions()
+		{
+            /*if holding phone upside down it will try to rotate it and get the value*/
+            //AutoRotate= true
+
+            /*which format it should scan*/
+            //Formats = {}
+
+            /*detect all barcodes*/
+            //Formats=BarcodeFormats.OneDimensional
+
+            /*tries harder to detect the barcodes,chech documentation*/
+            //TryHarder= true	
+        };
+	}
+
+	private void CameraBarcode_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+	{
+		//barcode scanning is happening on a background thread so the ui needs to be updated in the main thread
+		//Dispatcher helps to dispatch and go to the main thread so it doesn't crash
+		Dispatcher.Dispatch (() =>
+		{
+			/*Results[0] is a collection
+            inspect from one barcode(result) the value,raw data*/
+			//Result.Text = e.Results[0].Value;
+
+			//Included to see the format of the QR
+			Result.Text = $"{e.Results[0].Value} {e.Results[0].Format}";
+        });
+
+	}
+
+
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using ZXing.Net.Maui;
+
+namespace BarcodeScanner;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseBarcodeReader()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+
+		//hidden region which configures handlers 
+		//because whenever you point the camera at QR code it would do nothing
+		#region
+		.ConfigureMauiHandlers(h =>
+			{
+
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
+            });
+        #endregion
+
+        return builder.Build();
+	}
+}
